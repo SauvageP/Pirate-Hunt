ad260cbb7c190544fc05c65ac8782921a3430e68
yung305 <prgabriel88@gmail.com>
2014-10-28 18:24:48 -0400

    Initial Commit

---
+++ Pirate Hunt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Hunt
+//
+//  Created by Perry R Gabriel on 10/28/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Hunt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Hunt
+//
+//  Created by Perry R Gabriel on 10/28/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
